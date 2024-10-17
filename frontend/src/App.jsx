d998106c339340d67545d0dd4c3fae494c8c7432
Girish_Gaikwad <145460775+girish-gaikwad@users.noreply.github.com>
2024-10-17 09:25:55 +0530

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/route.jsx";
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
